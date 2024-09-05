e3c948e6be47bc7bd7581ac088f161b3762a4bfb
Kamil Kiryluk <kamilkiryluk@MacBook-Pro-Kamil.local>
2024-09-05 18:55:02 +0100

    added material icons and fixed tailwind for libs

--- libs/header/src/lib/header/header.component.ts
+++ libs/header/src/lib/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'lib-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
