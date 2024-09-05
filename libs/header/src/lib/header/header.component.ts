5c333dab936c1b39781475215ccf2be75dd09a38
Kamil Kiryluk <kamilkiryluk@MacBook-Pro-Kamil.local>
2024-09-05 19:33:13 +0100

    header styling

--- libs/header/src/lib/header/header.component.ts
+++ libs/header/src/lib/header/header.component.ts
@@ -7,6 +7,5 @@ import { MatIconModule } from '@angular/material/icon';
   standalone: true,
   imports: [CommonModule, MatIconModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css',
 })
 export class HeaderComponent {}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{html,ts}', './libs/**/*.{html,ts}'],
   theme: {
+    fontFamily: {
+      sans: ['"Inter"', 'sans-serif'],
+    },
+    colors: {
+      textPrimary: '#1B1C1E',
+    },
     extend: {},
   },
   plugins: [],
